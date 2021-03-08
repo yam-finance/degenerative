766c8e659d456e857ad22463bc934e82c2e4aae9
sali <saadali93@gmail.com>
2021-03-07 19:37:51 -0600

    Updated landing page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,97 +1,120 @@
 <template>
   <div class="home">
-    <div class="home-content">
+    <Container :size="900">
+      <img class="banner" src="../assets/graphics/degen_city.png" alt="" />
+      <div class="getdegenerative degenerative">
+        <div class="large-title">Get Degenerative</div>
+        <p class="subtitle">Cutting Edge DeFi Derivatives.</p>
+      </div>
+      <div class="row">
+        <div class="col">
+          <p>
+            Introducing uSTONKS, a synthetic that tracks the ten most bullish Wall Street Bets stocks and captures the sentiment of the r/WSB community.
+          </p>
+          <p><b>Mint + Stake uSTONKS</b> – Earn High APR Rewards.</p>
+          <p>
+            Bonus #1: Extra 5,000 $YAM and 5,000 $UMA tokens will be distributed as liquidity mining rewards from March 8th to 14th.
+          </p>
+          <p>
+            Bonus #2: Extra 10,000 $YAM and 10,000 $UMA tokens will be distributed as liquidity mining rewards from March 15th to 29th.
+          </p>
+        </div>
+
+        <div class="col">
+          <p>
+            Check out uGAS, our synthetic futures contract that settles to the 30-Day Median Ethereum Gas Price.
+          </p>
+          <p>
+            Speculate on Ethereum gas prices with a Long or Short uGAS position. uGAS can be used as a hedge against the cost of gas on the Ethereum blockchain.
+          </p>
+          <p><b>Mint + Stake uGAS</b> – Earn High APR Rewards.</p>
+          <p>
+            Mint uGAS and provide liquidity to the uGAS/ETH pool to earn $YAM & $UMA Rewards.
+          </p>
+        </div>
+      </div>
+    </Container>
+
+    <div class="home-details">
       <Container :size="900">
-        <div class="getdegenerative">
-          <div class="large-title">Get Degenerative</div>
-          <div class="side">
-            <p class="subtitle">Cutting Edge DeFi Derivatives.</p>
-            <p class="subtitle sub2">
-              Built by
-              <b> <a href="https://yam.finance/" target="_blank">Yam</a> </b>. Built on <b> <a href="https://umaproject.org/" target="_blank">UMA</a> </b>.
-              Built for Degens.
-            </p>
-            <p class="justify">
-              Introducing uGas, a synthetic futures contract that settles to the 30-Day Median Gas Price at the end of each month. If you’re just dying to try
-              it, click here to <a href="https://yamfinance.medium.com/degenerative-finance-ugas-user-guide-9d2622dde72" target="_blank">learn more</a>.
-              <br />
-              <br />
-            </p>
-            <router-link class="grph" to="/assets">Launch</router-link>
+        <div>
+          <h2>How to use Degenerative</h2>
+          <Space size="sm" />
+
+          <div class="row">
+            <div class="item">
+              <img src="../assets/graphics/graphic_create.png" />
+              <h3>Create</h3>
+              <div class="text justify">
+                Deposit ETH as collateral to mint uGAS tokens. Each uGas token represents 1,000,000 gas and expires at 00:00 UTC on the 1st day of the following
+                month. You can withdraw collateral at any time as long as the Minimum Collateral Ratio is maintained.
+              </div>
+            </div>
+
+            <Space size="64" />
+
+            <div class="item">
+              <img src="../assets/graphics/graphic_trade.png" />
+              <h3>Trade</h3>
+              <div class="text justify">
+                Hold your uGAS tokens until the expiry date or trade them on your favorite DEX to pocket the difference between your minting cost and the uGAS
+                spot price as profit.
+              </div>
+            </div>
+          </div>
+
+          <Space size="32" />
+
+          <div class="row">
+            <div class="item">
+              <img src="../assets/graphics/graphic_liqudity.png" />
+              <h3>Provide Liquidity</h3>
+              <div class="text justify">
+                Mint and deposit your uGAS tokens in the Uniswap uGAS/ETH LP to earn UMA liquidity provider rewards. To qualify, users must both mint and
+                deposit their uGAS tokens. Tokens which are traded and then deposited will not qualify.
+              </div>
+            </div>
+
+            <Space size="64" />
+
+            <div class="item">
+              <img src="../assets/graphics/graphic_redeem.png" />
+              <h3>Redeem</h3>
+              <div class="text justify">
+                Redeem your uGAS tokens upon expiry for ETH. The settlement price for uGAS is based on the rolling 30-day median price of all Ethereum
+                transactions.
+              </div>
+            </div>
           </div>
         </div>
       </Container>
-
-      <div class="home-details">
-        <Container :size="900">
-          <div>
-            <h2>How to use Degenerative</h2>
-            <Space size="sm" />
-
-            <div class="row">
-              <div class="item">
-                <img src="../assets/graphics/graphic_create.png" />
-                <h3>Create</h3>
-                <div class="text justify">
-                  Deposit ETH as collateral to mint uGAS tokens. Each uGas token represents 1,000,000 gas and expires at 00:00 UTC on the 1st day of the
-                  following month. You can withdraw collateral at any time as long as the Minimum Collateral Ratio is maintained.
-                </div>
-              </div>
-
-              <Space size="64" />
-
-              <div class="item">
-                <img src="../assets/graphics/graphic_trade.png" />
-                <h3>Trade</h3>
-                <div class="text justify">
-                  Hold your uGAS tokens until the expiry date or trade them on your favorite DEX to pocket the difference between your minting cost and the uGAS
-                  spot price as profit.
-                </div>
-              </div>
-            </div>
-
-            <Space size="32" />
-
-            <div class="row">
-              <div class="item">
-                <img src="../assets/graphics/graphic_liqudity.png" />
-                <h3>Provide Liquidity</h3>
-                <div class="text justify">
-                  Mint and deposit your uGAS tokens in the Uniswap uGAS/ETH LP to earn UMA liquidity provider rewards. To qualify, users must both mint and
-                  deposit their uGAS tokens. Tokens which are traded and then deposited will not qualify.
-                </div>
-              </div>
-
-              <Space size="64" />
-
-              <div class="item">
-                <img src="../assets/graphics/graphic_redeem.png" />
-                <h3>Redeem</h3>
-                <div class="text justify">
-                  Redeem your uGAS tokens upon expiry for ETH. The settlement price for uGAS is based on the rolling 30-day median price of all Ethereum
-                  transactions.
-                </div>
-              </div>
-            </div>
-          </div>
-        </Container>
-      </div>
     </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
 .home {
-  background: url("../assets/graphics/city.svg") no-repeat center -60px;
-  background-size: 1420px 2254px; // 1360px 2164px;
-  min-height: 900px;
-  @media (max-width: 420px) {
-    background-size: 1420px 2564px;
-  }
+  max-width: 900px;
 }
-.home-content {
-  z-index: 2;
-  position: relative;
+.banner {
+  padding-bottom: 20px;
+  width: 100%;
+  margin: auto;
+}
+.home-columns {
+  display: flex;
+  flex-direction: row;
+}
+.col {
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  margin-left: 20px;
+  margin-right: 20px;
+
+  p {
+    margin-bottom: 20px;
+  }
 }
 .degenerative {
   font-size: 94px;
@@ -106,20 +129,10 @@
 }
 
 .getdegenerative {
-  min-height: 540px;
-  @media (max-width: 420px) {
-    min-height: 620px;
-  }
+  padding-bottom: 20px;
 
   .large-title {
-    font-size: 65px;
-
-    @media (max-width: 690px) {
-      font-size: 54px;
-    }
-    @media (max-width: 420px) {
-      font-size: 38px;
-    }
+    font-size: 54px;
   }
   .subtitle {
     font-size: 28px;
